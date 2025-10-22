5524aeab9aa7a40f0a0c5d42ac90696fabffefae
Farouk Umar Sedick <sadikalhanssah@gmail.com>
2025-10-22 21:08:16 +0000

    feat: config users microservice for docker network

--- eCommerce.Infrastructure/DbContext/DapperDbContext.cs
+++ eCommerce.Infrastructure/DbContext/DapperDbContext.cs
@@ -6,14 +6,15 @@ namespace eCommerce.Infrastructure.DbContext;
 
 public class DapperDbContext
 {
-    private readonly IConfiguration _configuration;
     private readonly IDbConnection _connection;
     
     public DapperDbContext(IConfiguration configuration)
     {
-        _configuration = configuration;
-        
-        string connectionString = _configuration.GetConnectionString("PostgresConnection");
+        string connectionStringTemplate = configuration.GetConnectionString("PostgresConnection")!;
+
+        string connectionString = connectionStringTemplate
+            .Replace("$POSTGRES_HOST", Environment.GetEnvironmentVariable("POSTGRES_HOST"))
+            .Replace("$POSTGRES_PASSWORD", Environment.GetEnvironmentVariable("POSTGRES_PASSWORD"));
         
         // Create a new NpgsqlConnection with retrieved connection string
         _connection = new NpgsqlConnection(connectionString);
